4e6e394aa480c3b8ee873bb16452ea8be95c02e0
jottado7 <jpaulo25416@gmail.com>
2026-03-30 10:01:07 -0300

    Update e.py

--- e.py
+++ e.py
@@ -6,11 +6,4 @@ peso = float(input("{}, digite seu peso em kg: ".format(nome)))
 altura = float(input("{}, digite sua altura em metros: ".format(nome)))
 imc = peso / (altura ** 2)
 print("{}, seu IMC é: {:.2f}".format(nome, imc))
-if imc < 18.5:
-    print("{}, você está abaixo do peso.".format(nome))
-elif imc < 25:
-    print("{}, você está com peso normal.".format(nome))
-elif imc < 30:
-    print("{}, você está com sobrepeso.".format(nome))
-else:
-    print("{}, você está com obesidade.".format(nome))
+
