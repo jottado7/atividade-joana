2fd2c261bd6fb3442a7ff689ad24e51949b90d57
jottado7 <jpaulo25416@gmail.com>
2026-03-31 07:37:32 -0300

    Update g.py

--- g.py
+++ g.py
@@ -1,6 +1,6 @@
-#Algoritmo que calcula salário total de um funcionário  
-horas_trabalhadas = float(input("Digite o número de horas trabalhadas: "))
-valor_hora = float(input("Digite o valor da hora trabalhada: "))
-salario_total = horas_trabalhadas * valor_hora
-print("O salário total é: R$ {:.2f}".format(salario_total))
+# calcular salario
 
+h = float(input("digite as horas: "))
+v = float(input("digite o valor da hora: "))
+s = h * v
+print("salario =", s)
