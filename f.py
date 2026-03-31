9c27b67c13cb352842b8f83a96c78ef04b363411
jottado7 <jpaulo25416@gmail.com>
2026-03-31 07:35:44 -0300

    Refactor Celsius to Fahrenheit conversion
    Updated temperature conversion algorithm for clarity.

--- f.py
+++ f.py
@@ -1,4 +1,5 @@
-#Algoritmo para converter temperatura de Celsius para Fahrenheit
-celsius = float(input("Digite a temperatura em Celsius: "))
-fahrenheit = (celsius * 1.8) + 32
-print("A temperatura em Fahrenheit é: {:.2f}".format(fahrenheit))
+# converter celsius para fahrenheit
+
+c = float(input("digite a temperatura: "))
+f = (c * 1.8) + 32
+print("fahrenheit =", f)
