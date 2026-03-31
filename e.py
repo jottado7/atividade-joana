4dde7641af693b62eb1d1e8ff78997d58ef9c90c
jottado7 <jpaulo25416@gmail.com>
2026-03-31 07:34:49 -0300

    Refactor IMC calculator for clarity and simplicity

--- e.py
+++ e.py
@@ -1,9 +1,8 @@
-#Algoritmo para calcular IMC
-print("Bem-vindo ao calculador de IMC!")
-nome = input("Digite seu nome: ")
-print("Olá, {}!".format(nome) + " Este programa irá calcular o seu Índice de Massa Corporal (IMC).")
-peso = float(input("{}, digite seu peso em kg: ".format(nome)))
-altura = float(input("{}, digite sua altura em metros: ".format(nome)))
-imc = peso / (altura ** 2)
-print("{}, seu IMC é: {:.2f}".format(nome, imc))
+# calculo de imc
 
+print("calculadora de imc")
+nome = input("digite seu nome: ")
+peso = float(input("digite seu peso: "))
+altura = float(input("digite sua altura: "))
+imc = peso / (altura * altura)
+print(nome, "seu imc =", imc)
